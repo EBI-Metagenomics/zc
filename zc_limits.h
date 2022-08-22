840ff6a3ac7116d0d6e4dcdd0caef53070613d6b
Danilo Horta <danilo.horta@pm.me>
2022-08-22 22:42:58 +0100

    Use width for bits, remove intptr info

--- zc_limits.h
+++ zc_limits.h
@@ -6,66 +6,50 @@
 
 #if SHRT_MAX == INT8_MAX
 #define ZC_BYTES_PER_SHORT 1
-#define ZC_BITS_PER_SHORT 8
+#define ZC_SHORT_WIDTH 8
 #elif SHRT_MAX == INT16_MAX
 #define ZC_BYTES_PER_SHORT 2
-#define ZC_BITS_PER_SHORT 16
+#define ZC_SHORT_WIDTH 16
 #elif SHRT_MAX == INT32_MAX
 #define ZC_BYTES_PER_SHORT 4
-#define ZC_BITS_PER_SHORT 32
+#define ZC_SHORT_WIDTH 32
 #elif SHRT_MAX == INT64_MAX
 #define ZC_BYTES_PER_SHORT 8
-#define ZC_BITS_PER_SHORT 64
+#define ZC_SHORT_WIDTH 64
 #else
 #error "Cannot determine size of short"
 #endif
 
 #if INT_MAX == INT8_MAX
 #define ZC_BYTES_PER_INT 1
-#define ZC_BITS_PER_INT 8
+#define ZC_INT_WIDTH 8
 #elif INT_MAX == INT16_MAX
 #define ZC_BYTES_PER_INT 2
-#define ZC_BITS_PER_INT 16
+#define ZC_INT_WIDTH 16
 #elif INT_MAX == INT32_MAX
 #define ZC_BYTES_PER_INT 4
-#define ZC_BITS_PER_INT 32
+#define ZC_INT_WIDTH 32
 #elif INT_MAX == INT64_MAX
 #define ZC_BYTES_PER_INT 8
-#define ZC_BITS_PER_INT 64
+#define ZC_INT_WIDTH 64
 #else
 #error "Cannot determine size of int"
 #endif
 
 #if LONG_MAX == INT8_MAX
 #define ZC_BYTES_PER_LONG 1
-#define ZC_BITS_PER_LONG 8
+#define ZC_LONG_WIDTH 8
 #elif LONG_MAX == INT16_MAX
 #define ZC_BYTES_PER_LONG 2
-#define ZC_BITS_PER_LONG 16
+#define ZC_LONG_WIDTH 16
 #elif LONG_MAX == INT32_MAX
 #define ZC_BYTES_PER_LONG 4
-#define ZC_BITS_PER_LONG 32
+#define ZC_LONG_WIDTH 32
 #elif LONG_MAX == INT64_MAX
 #define ZC_BYTES_PER_LONG 8
-#define ZC_BITS_PER_LONG 64
+#define ZC_LONG_WIDTH 64
 #else
 #error "Cannot determine size of long"
-#endif
-
-#if INTPTR_MAX == INT8_MAX
-#define ZC_BYTES_PER_POINTER 1
-#define ZC_BITS_PER_POINTER 8
-#elif INTPTR_MAX == INT16_MAX
-#define ZC_BYTES_PER_POINTER 2
-#define ZC_BITS_PER_POINTER 16
-#elif INTPTR_MAX == INT32_MAX
-#define ZC_BYTES_PER_POINTER 4
-#define ZC_BITS_PER_POINTER 32
-#elif INTPTR_MAX == INT64_MAX
-#define ZC_BYTES_PER_POINTER 8
-#define ZC_BITS_PER_POINTER 64
-#else
-#error "Cannot determine size of void *"
 #endif
 
 #endif
