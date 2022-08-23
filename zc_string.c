5359922017b488f5a39a9968bdbdfe766c64ae43
Danilo Horta <danilo.horta@pm.me>
2022-08-23 06:46:43 +0100

    strlcat

--- zc_string.c
+++ zc_string.c
@@ -13,6 +13,42 @@ char *zc_strdup(char const *str)
 }
 
 /*
+ * Appends src to string dst of size dsize (unlike strncat, dsize is the
+ * full size of dst, not space left).  At most dsize-1 characters
+ * will be copied.  Always NUL terminates (unless dsize <= strlen(dst)).
+ * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
+ * If retval >= dsize, truncation occurred.
+ *
+ * Copyright (c) 1998, 2015 Todd C. Miller <Todd.Miller@courtesan.com>
+ */
+size_t zc_strlcat(char *dst, char const *src, size_t dsize)
+{
+    const char *odst = dst;
+    const char *osrc = src;
+    size_t n = dsize;
+    size_t dlen;
+
+    while (n-- != 0 && *dst != '\0')
+        dst++;
+    dlen = dst - odst;
+    n = dsize - dlen;
+
+    if (n-- == 0) return (dlen + strlen(src));
+    while (*src != '\0')
+    {
+        if (n != 0)
+        {
+            *dst++ = *src;
+            n--;
+        }
+        src++;
+    }
+    *dst = '\0';
+
+    return (dlen + (src - osrc));
+}
+
+/*
  * Copyright (c) 1998, 2015 Todd C. Miller <Todd.Miller@courtesan.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
--- zc_string.h
+++ zc_string.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 char *zc_strdup(const char *s);
+size_t zc_strlcat(char *dst, char const *src, size_t dsize);
 size_t zc_strlcpy(char *dst, char const *src, size_t dsize);
 
 #endif
